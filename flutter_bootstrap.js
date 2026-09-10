24b86631f205f903c9de6df2a205ee55f4655f99
Abject <maxime.radomski@gmail.com>
2026-09-10 18:28:29 +0200

    fix: Authentication clean

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"8a9f61cfd67396fb2f9afc3cd7854035e9cd6f
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1967736386" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3136044016" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
